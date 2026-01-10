d0ebb84d472f97ac86284bfa664c6f5c2a53fb49
HaKu <haku@sumemo.dev>
2026-01-09 21:44:01 -0500

    refactor: enhance global using directives and improve EditorConfig settings

--- MemoUploader/GlobalUsings/ImGui.cs
+++
@@ -1,7 +0,0 @@
-﻿global using Dalamud.Bindings.ImGui;
-global using Dalamud.Bindings.ImGuizmo;
-global using Dalamud.Bindings.ImPlot;
-global using OmenTools.ImGuiOm;
-global using Dalamud.Interface;
-global using Dalamud.Interface.Utility.Raii;
-global using System.Drawing;
