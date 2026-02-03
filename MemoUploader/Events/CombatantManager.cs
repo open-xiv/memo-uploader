352d61b01c7b5880158eb31fd0bb5e902a5da72b
HaKu <haku@sumemo.dev>
2026-02-03 11:48:11 -0500

    impl: remove combatant table events

--- MemoUploader/Events/CombatantManager.cs
+++ MemoUploader/Events/CombatantManager.cs
@@ -4,24 +4,20 @@ using System.Linq;
 using Dalamud.Plugin.Services;
 using MemoEngine;
 using MemoEngine.Models;
-using ObjectKind = Dalamud.Game.ClientState.Objects.Enums.ObjectKind;
 
 
 namespace MemoUploader.Events;
 
 public class CombatantManager
 {
-    private readonly List<IGameObject>                lastCombatants = [];
-    private readonly Dictionary<uint, CombatantState> lastStates     = [];
-
-    private record CombatantState(bool IsTargetable, bool IsDead);
+    private readonly Dictionary<uint, bool> lastAlive = [];
 
     public void Init() => FrameworkManager.Instance().Reg(OnFrameworkUpdate, 500);
 
     public void Uninit()
     {
         FrameworkManager.Instance().Unreg(OnFrameworkUpdate);
-        lastCombatants.Clear();
+        lastAlive.Clear();
     }
 
     private void OnFrameworkUpdate(IFramework framework)
@@ -29,54 +25,21 @@ public class CombatantManager
         if (Context.Lifecycle is EngineState.Idle)
             return;
 
-        // combatants
-        var currentCombatants = DService.Instance().ObjectTable.Where(x => x.ObjectKind is ObjectKind.BattleNpc).ToList();
+        if (Context.EnemyDataId != 0 && DService.Instance().ObjectTable.FirstOrDefault(x => x.DataID == Context.EnemyDataId) is IBattleChara enemy)
+            Event.Combatant.RaiseHpUpdated(DateTimeOffset.UtcNow, enemy.DataID, enemy.CurrentHp, enemy.MaxHp);
 
-        // spawn
-        foreach (var obj in currentCombatants.Except(lastCombatants))
-            Event.Combatant.RaiseSpawned(DateTimeOffset.UtcNow, obj.DataID);
+        var currentAlive = new Dictionary<uint, bool>();
+        if (DService.Instance().PartyList.Length >= 1)
+            currentAlive = DService.Instance().PartyList.ToDictionary(p => p.EntityId, p => p.GameObject is not null && !p.GameObject.IsDead && p.CurrentHP > 0);
 
-        // destroy
-        foreach (var obj in lastCombatants.Except(currentCombatants))
-            Event.Combatant.RaiseDestroyed(DateTimeOffset.UtcNow, obj.DataID);
-
-        // update combatants
-        lastCombatants.Clear();
-        lastCombatants.AddRange(currentCombatants);
-
-        // last states
-        var currentStates = DService.Instance().ObjectTable
-                                    .Where(x => x.ObjectKind is ObjectKind.Player or ObjectKind.BattleNpc)
-                                    .DistinctBy(x => x.EntityID)
-                                    .ToDictionary(x => x.EntityID, x => x);
-
-        // status changes
-        foreach (var (entityId, currentState) in currentStates)
+        foreach (var (entityId, isAlive) in currentAlive)
         {
-            if (!lastStates.TryGetValue(entityId, out var lastState))
-                continue;
-
-            // targetable and untargetable
-            switch (lastState.IsTargetable)
-            {
-                case true when !currentState.IsTargetable:
-                    Event.Combatant.RaiseBecameTargetable(DateTimeOffset.UtcNow, currentState.DataID);
-                    break;
-                case false when currentState.IsTargetable:
-                    Event.Combatant.RaiseBecameUntargetable(DateTimeOffset.UtcNow, currentState.DataID);
-                    break;
-            }
-
-            // death
-            if (currentState.ObjectKind is not ObjectKind.Player || lastState.IsDead || !currentState.IsDead)
-                continue;
-
-            Event.General.RaisePlayerDied(DateTimeOffset.UtcNow, currentState.EntityID);
+            if (lastAlive.TryGetValue(entityId, out var wasAlive) && wasAlive && !isAlive)
+                Event.General.RaisePlayerDied(DateTimeOffset.UtcNow, entityId);
         }
 
-        // update states
-        lastStates.Clear();
-        foreach (var obj in currentStates.Values)
-            lastStates[obj.EntityID] = new CombatantState(obj.IsTargetable, obj.IsDead);
+        lastAlive.Clear();
+        foreach (var (entityId, isAlive) in currentAlive)
+            lastAlive[entityId] = isAlive;
     }
 }
